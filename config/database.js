d585207f1e679e8ab965ec5d757ee8e680a00fad
Admin <admin@111-MBA.local>
2015-11-19 13:16:59 +0100

    v2

--- config/database.js
+++ config/database.js
@@ -1,3 +1,1 @@
-module.exports = {
-    url : 'mongodb://test1:1tset@ds057244.mongolab.com:57244/tuto'    
-};
+module.exports = {   url : 'mongodb://test1:1tset@ds057244.mongolab.com:57244/tuto'    };
